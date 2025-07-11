8e4b50e0e5e06779bd95c104fb4fb13c3e3b2e28
Trey E. Shenk <trey.shenk@gmail.com>
2025-07-11 08:31:07 -0700

    hooks for different lag loss metrics

--- src/LagLoss.jl
+++ src/LagLoss.jl
@@ -1,9 +1,15 @@
 include("Utils.jl")
 
+abstract type MetricType end
+struct MSE <: MetricType end
+struct Correlation <: MetricType end
+
+
 function lag_loss(
     ref::AbstractVector{<:Real},
     sig::AbstractVector{<:Real},
     lag::Int,
+    ::MSE
 )
     n = length(ref)
     
@@ -18,14 +24,40 @@ function lag_loss(
         sig_view = view(sig, 1:n)
     end
 
-    sum((ref_view - sig_view).^2)
+    return sum((ref_view - sig_view).^2)
 end
+
+function lag_loss(
+    ref::AbstractVector{<:Real},
+    sig::AbstractVector{<:Real},
+    lag::Int,
+    ::Correlation
+)
+    n = length(ref)
+    
+    if lag > 0
+        ref_view = view(ref, (1+lag):n)
+        sig_view = view(sig, 1:(n-lag))
+    elseif lag < 0
+        ref_view = view(ref, 1:(n+lag))
+        sig_view = view(sig, (1-lag):n)
+    else
+        ref_view = view(ref, 1:n)
+        sig_view = view(sig, 1:n)
+    end
+
+    return sum(ref_view.*conj.(sig_view))
+end
+
+best_lag_selector(::MSE) = argmin
+best_lag_selector(::Correlation) = argmax
 
 function find_optimal_lag(
     ref::AbstractVector{<:Real},
     sig::AbstractVector{<:Real};
     min_lag::Union{Int, Nothing} = nothing,
-    max_lag::Union{Int, Nothing} = nothing
+    max_lag::Union{Int, Nothing} = nothing,
+    lag_metric::MetricType = MSE()
 )
     if length(ref) != length(sig)
         error("ref and sig must have the same length")
@@ -58,13 +90,13 @@ function find_optimal_lag(
     all_losses = zeros(max_lag - min_lag + 1)
     all_lags = min_lag:max_lag
     for (index, lag) in enumerate(all_lags)
-            all_losses[index] = lag_loss(ref, sig, lag)
+            all_losses[index] = lag_loss(ref, sig, lag, lag_metric)
     end
+    selector = best_lag_selector(lag_metric)
 
-    min_ind = argmin(all_losses)
+    min_ind = selector(all_losses)
 
-    #TODO Peak interpolation can be performed here
-
+    #TODO Take care of the edge cases
     peak_adjust = peak_interp_quad(all_losses[(min_ind-1):min_ind+1])
 
     best_lag = all_lags[min_ind] + peak_adjust
