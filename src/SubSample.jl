0690705ff143435d282a2db220c22c8f8318e27a
Trey E. Shenk <trey.shenk@gmail.com>
2025-07-11 22:30:09 -0700

    additional subsample filter method

--- src/SubSample.jl
+++ src/SubSample.jl
@@ -3,12 +3,21 @@ using DSP
 abstract type SubsampleShiftMethod end
 
 struct LagrangeInterpolation <: SubsampleShiftMethod
-    order::Int
+    N::Int
 end
 
-function subsample_shift(x, D, m::LagrangeInterpolation)
+struct ThiranAllPass <: SubsampleShiftMethod
+    N::Int
+end
+
+function subsample_shift(x, D, m::SubsampleShiftMethod)
+    h = calc_filter(D, m)
+    output = conv(x, h)
+end
+
+function calc_filter(D, m::LagrangeInterpolation)
     # Construct and apply filter
-    N = m.order
+    N = m.N
     h = ones(Float64, N+1)
     for n in 0:N, k in 0:N
         if k != n
@@ -16,10 +25,20 @@ function subsample_shift(x, D, m::LagrangeInterpolation)
             h[n+1] = h[n+1]*new_factor
         end
     end
+    return h
+end
 
-    output = conv(x, h)
+function calc_filter(D, m::ThiranAllPass)
+    N = m.N
+    a = ones(Float64, N+1)
+    for k in 0:N
+        a[k+1] = -1*binomial(N, k)
+    end
 
-    #TODO See if this needs to be trimmed to match the length of x
-    
-    return output
+    for k in 0:N, n = 0:N
+        new_factor = (D - N + n)/(D - N + k + n)
+        a[k+1] = a[k+1]*new_factor
+    end
+
+    return a
 end
