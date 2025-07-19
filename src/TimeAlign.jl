d14f55a6bdf96e8df4d0d44153dccafa337f91ef
Trey E. Shenk <trey.shenk@gmail.com>
2025-07-19 12:52:29 -0700

    exporting shift estimators

--- src/TimeAlign.jl
+++ src/TimeAlign.jl
@@ -2,6 +2,6 @@ module TimeAlign
 
 include("Aligner.jl")
 
-export align_signals_naive, align_signals_complete
+export align_signals_naive, align_signals_complete, calc_alignment_naive, calc_alignment_complete
 
 end
