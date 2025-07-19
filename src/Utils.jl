c30491bc78fe210c19d77096eb16d73a952bea49
Trey E. Shenk <trey.shenk@gmail.com>
2025-07-19 12:52:57 -0700

    removed placeholder

--- src/Utils.jl
+++ src/Utils.jl
@@ -1,9 +1,5 @@
 
-#TODO: Look at paper on "Splitting the Unit Delay" for methods to perform a subsample shift
-function subsample_shift(signal, shift)
-end
-
-#TODO: Implement from https://ccrma.stanford.edu/~jos/sasp/Quadratic_Interpolation_Spectral_Peaks.html
+#Implemented from https://ccrma.stanford.edu/~jos/sasp/Quadratic_Interpolation_Spectral_Peaks.html
 function peak_interp_quad(signal_peak)
     @assert length(signal_peak)==3
     α = signal_peak[1]
